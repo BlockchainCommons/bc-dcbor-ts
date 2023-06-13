5f899976ea53303da70a113eb58c9bf183892982
Wolf McNally <wolf@wolfmcnally.com>
2023-06-12 22:18:03 -0700

    Working.

--- src/map.spec.ts
+++ src/map.spec.ts
@@ -23,8 +23,8 @@ describe('CborMap', () => {
     expect(cborDebug(mapCbor)).toBe('map({0x0a: (unsigned(10), unsigned(1)), 0x1864: (unsigned(100), unsigned(2)), 0x20: (negative(-1), unsigned(3)), 0x617a: (text("z"), unsigned(4)), 0x626161: (text("aa"), unsigned(5)), 0x811864: (array([unsigned(100)]), unsigned(6)), 0x8120: (array([negative(-1)]), unsigned(7)), 0xf4: (simple(false), unsigned(8))})');
     expect(cborDiagnostic(mapCbor)).toBe('{10: 1, 100: 2, -1: 3, "z": 4, "aa": 5, [100]: 6, [-1]: 7, false: 8}');
 
-    let encoded = cborData(mapCbor);
-    let decodedCbor = decodeCbor(encoded);
+    const encoded = cborData(mapCbor);
+    const decodedCbor = decodeCbor(encoded);
 
     expect(cborDebug(mapCbor)).toEqual(cborDebug(decodedCbor));
     expect(cborDiagnostic(mapCbor)).toEqual(cborDiagnostic(decodedCbor));
@@ -45,18 +45,18 @@ describe('CborMap', () => {
 
     const mapCbor = cbor(map);
     expect(cborDiagnostic(mapCbor)).toBe('{10: 1, 100: 2, -1: 3, "z": 4, "aa": 5, [100]: 6, [-1]: 7, false: 8}');
-    let encoded = cborData(mapCbor);
-    let decoded = decodeCbor(encoded);
+    const encoded = cborData(mapCbor);
+    const decoded = decodeCbor(encoded);
     expect(cborDiagnostic(mapCbor)).toEqual(cborDiagnostic(decoded));
     expect((extractCbor(decoded) as CborMap).toMap()).toEqual(map);
   });
 
   test('Anders map', () => {
-    let map = new Map<any, any>([
+    const map = new Map<any, any>([
       [1, 45.7],
       [2, 'Hi there!']
     ]);
-    expect(cborHex(map)).toBe('a201fb4046d9999999999a0269486920746865726521')
+    expect(cborHex(map)).toBe('a201fb4046d9999999999a0269486920746865726521');
     expect(map.get(1)).toBe(45.7);
   });
 
